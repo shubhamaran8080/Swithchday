44b0c39bbb0c17a75944cb4e6256be9ee3c583ec
shubhamaran8080 <166980856+shubhamaran8080@users.noreply.github.com>
2026-03-03 19:49:18 +0530

    Add files via upload

---
+++ switchday.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class switchday{
+    public static void main(String[] args) {
+        
+Scanner sc = new Scanner(System.in);
+System.out.println("enter number");
+int num = sc.nextInt();
+
+       switch(num){
+            case 1:
+            System.out.println("monday");
+            break;
+             case 2:
+            System.out.println("tuesday");
+            break;
+             case 3:
+            System.out.println("wednesday");
+            break;
+             case 4:
+            System.out.println("thursday");
+            break;
+             case 5:
+            System.out.println("friday");
+            break;
+             case 6:
+            System.out.println("saturday");
+            break;
+             case 7:
+            System.out.println("sunday");
+            break;
+            default:
+            System.out.println("invalid");
+            break;
+            
+       }
+
+
+    }
+}
